35814d3c48650cc5846c1bba42e29c407994ea59
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2017-03-08 18:41:23 +0100

    IDGenerator struct

--- util.go
+++ util.go
@@ -5,21 +5,43 @@ import (
 	"strconv"
 )
 
-var index uint64 = 1
-
-// UniqID returns and increments the current index
-func UniqID() uint64 {
-	res := index
-	index++
-	return res
-}
-
 // FormatID returns a new 'random' string id
 func FormatID(id uint64) string {
 	return fmt.Sprintf("%03s", strconv.FormatUint(id*46649%6125, 36))
 }
 
+// IDGenerator can be used to generate unique IDs
+type IDGenerator struct {
+	index uint64
+}
+
+// MakeIDGenerator makes an IDGenerator
+func MakeIDGenerator() IDGenerator {
+	return IDGenerator{
+		index: 0,
+	}
+}
+
+// UniqID returns and increments the current index
+func (g *IDGenerator) UniqID() uint64 {
+	res := g.index
+	g.index++
+	return res
+}
+
+// UniqIDf returns a new 'random' string id and increaes the index
+func (g *IDGenerator) UniqIDf() string {
+	return FormatID(g.UniqID())
+}
+
+var gen = MakeIDGenerator()
+
+// UniqID returns and increments the current index
+func UniqID() uint64 {
+	return gen.UniqID()
+}
+
 // UniqIDf returns a new 'random' string id and increaes the index
 func UniqIDf() string {
-	return FormatID(UniqID())
+	return gen.UniqIDf()
 }
